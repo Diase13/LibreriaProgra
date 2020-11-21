6c5968aa56ecad6cb0267a37705009ec8726453b
Daniel151505 <daniel150520011@gmail.com>
2020-11-21 01:47:13 -0500

    Fix error name

--- Controllers/ContactenosController.cs
+++ Controllers/ContactenosController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using proga.Models;
 
-namespace proga.Controllers
+namespace LibreriaProgra.Controllers
 {
     public class ContactenosController : Controller
     {
