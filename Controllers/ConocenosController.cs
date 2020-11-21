33d3a7d84330681e740d92f21bb7188d9b8094ee
Daniel151505 <daniel150520011@gmail.com>
2020-11-21 01:49:21 -0500

    Fix name error

--- Controllers/ConocenosController.cs
+++ Controllers/ConocenosController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using proga.Models;
+using LibreriaProgra.Models;
 
 namespace LibreriaProgra.Controllers
 {
