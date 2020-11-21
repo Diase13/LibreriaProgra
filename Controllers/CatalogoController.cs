dc3b01ab40eed389d3c5b8b9f47269733c1e368d
Daniel151505 <daniel150520011@gmail.com>
2020-11-20 23:52:34 -0500

    Creando CatalogoController

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using proga.Models;
+
+namespace proga.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CatalogoController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
