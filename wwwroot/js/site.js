e3f94b8dc57cec0e6bdd3bf61ee8df24cbd6ca99
Daniel151505 <daniel150520011@gmail.com>
2020-11-21 01:32:23 -0500

    Añadiendo site funciones.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,125 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+!(function($) {
+    "use strict";
+  
+    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        e.preventDefault();
+        var target = $(this.hash);
+        if (target.length) {
+  
+          var scrollto = target.offset().top;
+  
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
+  
+          if ($(this).parents('.nav-menu, .mobile-nav').length) {
+            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+            $(this).closest('li').addClass('active');
+          }
+  
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+          return false;
+        }
+      }
+    });
+  
 
-// Write your JavaScript code.
-function clicked() { 
-    return confirm('Compra aceptada. Gracias por su compra'); 
-} 
+    if ($('.nav-menu').length) {
+      var $mobile_nav = $('.nav-menu').clone().prop({
+        class: 'mobile-nav d-lg-none'
+      });
+      $('body').append($mobile_nav);
+      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+      $('body').append('<div class="mobile-nav-overly"></div>');
+  
+      $(document).on('click', '.mobile-nav-toggle', function(e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        $('.mobile-nav-overly').toggle();
+      });
+  
+      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+        e.preventDefault();
+        $(this).next().slideToggle(300);
+        $(this).parent().toggleClass('active');
+      });
+  
+      $(document).click(function(e) {
+        var container = $(".mobile-nav, .mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+        }
+      });
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+      $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
+  
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+      }
+    });
+  
+    $('.back-to-top').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1500, 'easeInOutExpo');
+      return false;
+    });
+  
+
+    $(window).on('load', function() {
+      var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+  
+      $('#portfolio-flters li').on('click', function() {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+  
+        portfolioIsotope.isotope({
+          filter: $(this).data('filter')
+        });
+        aos_init();
+      });
+  
+      $(document).ready(function() {
+        $('.venobox').venobox();
+      });
+    });
+  
+
+    $(".portfolio-details-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
+  
+
+    function aos_init() {
+      AOS.init({
+        duration: 800,
+        easing: "ease-in-out",
+        once: true
+      });
+    }
+    $(window).on('load', function() {
+      aos_init();
+    });
+  
+  })(jQuery);
